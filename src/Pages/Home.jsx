1c899e86966e1fb8a930b075db586910b1b923f1
David Ikenna <sankkakeii@gmail.com>
2024-01-26 17:01:49 +0100

    debug navbar error

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,10 @@
 import Header from '../components/Header/Header';
 import About from '../components/About/About';
-import Services from './../sections/Services/Services';
+import Services from '../components/Services/Services';
 import Paradise from '../components/Paradise/Paradise';
 import Ourworks from '../components/Ourworks/Ourworks';
 import Partners from './../components/Partners/Partners';
-import Ourprocess from './../sections/Ourprocess/Ourprocess';
+import Ourprocess from '../components/Ourprocess/Ourprocess';
 import Contact from './../sections/Contact/Contact';
 import React from "react";
 
