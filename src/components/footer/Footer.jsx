9503e4dba59ed211243d59056a7f692c007269b2
ghostronin <osinachij161@gmail.com>
2024-02-13 10:31:50 +0100

    edit the background image for contact, our vision and footer components

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -79,8 +79,8 @@ const Footer = () => {
               </a>
               <a href="#">
                 <svg
-                  width="24"
-                  height="25"
+                  width="36"
+                  height="36"
                   viewBox="0 0 24 25"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
